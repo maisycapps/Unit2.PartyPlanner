5b48e82adaeb322e10c3848bf852b273ead85a12
Maisy Capps <maisycapps@gmail.com>
2024-08-12 11:12:07 -0700

    doc setup, initial assessment

---
+++ index.js
@@ -0,0 +1,12 @@
+const COHORT = "2407-FTB-ET-WEB-FT";
+const EVENTS_API_URL =  `https://fsa-crud-2aa9294fe819.herokuapp.com/api${COHORT}/events`;
+const GUESTS_API_URL = `https://fsa-crud-2aa9294fe819.herokuapp.com/api${COHORT}/guests`;
+const RSVPS_API_URL = `https://fsa-crud-2aa9294fe819.herokuapp.com/api${COHORT}/rsvps`;
+
+// render events
+    // ul of lis: each event card
+// add new event
+    // new event card innerHTML: event name, date, time, location, description
+    // submit button, eventListener, render changes when clicked
+// delete event
+    // button, eventListener, render changes when clicked
